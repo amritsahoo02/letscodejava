f7849b6a20d49e77848dd6e6addf301e34e82841
Matru Prasad Mishra <matru19042003@gmail.com>
2023-03-26 15:38:55 +0530

    initial commit

---
+++ odd_upto_100.java
@@ -0,0 +1,15 @@
+ class first {
+    
+{
+ public static void main(String args[])
+  {
+
+ 
+
+    for(int i=0;i<50;i++)
+     {
+       System.out.println("Odd values are "+(2*i+1));
+     }
+
+}}
+}
