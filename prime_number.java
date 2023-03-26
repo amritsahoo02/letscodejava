acdad2314f3e7b21bfb3d5234d50b1473d506d52
Matru Prasad Mishra <matru19042003@gmail.com>
2023-03-26 15:36:35 +0530

    initial commit

---
+++ prime_number.java
@@ -0,0 +1,27 @@
+
+class first
+{
+ public static void main(String args[])
+  {
+   int a=50;
+   boolean y=false;
+
+   for(int i=2;i<= a/2;i++)
+    {
+      if(a%i == 0)
+     {
+       y=true;
+       break;
+   }
+}
+
+
+ if(!y)
+ {
+  System.out.println("prime hai");
+ }
+ else
+{
+ System.out.println("non-Prime hai");
+}
+}}
